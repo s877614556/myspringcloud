ce14341f9d238ec00ff65b78425dab870f8c7f7b
tom_pro <goodmoring@maomao.com>
2019-05-24 14:27:37 +0800

    Initial commit

---
+++ myspringcloud-consumer-dept-80/src/main/java/com/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,25 @@
+package com.springcloud.cfgbeans;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean {
+    @Bean
+    @LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public IRule myRule(){
+        //return new RoundRobinRule();
+        return new RetryRule();// 测试这个策略需要关闭8002然后80访问几次后会发现8002被甩开了
+    }
+}
